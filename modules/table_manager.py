bfdb4a2d89f0ad0e0ee4de8dba15a513acabf1e9
LuchoLatella <lucho_latella@hotmail.com>
2024-08-22 21:21:34 -0300

    main  actualizado por gráfica

--- modules/table_manager.py
+++ modules/table_manager.py
@@ -1,22 +1,25 @@
-import pandas as pd
+import sqlite3
+from tkinter import messagebox
 
-import pandas as pd
+class TableManager:
+    def __init__(self, conn):
+        self.conn = conn
 
-def process_file(file):
-    try:
-        if file.filename.endswith('.csv'):
-            return pd.read_csv(file, encoding='utf-8', delimiter=',', quotechar='"', error_bad_lines=False)
-        else:
-            return pd.read_excel(file)
-    except UnicodeDecodeError:
-        if file.filename.endswith('.csv'):
-            return pd.read_csv(file, encoding='latin1', delimiter=',', quotechar='"', error_bad_lines=False)
-        else:
-            return pd.read_excel(file)
+    def create_table(self, table_name, columns):
+        """ Crea una tabla en la base de datos con las columnas especificadas. """
+        cursor = self.conn.cursor()
+        columns_definition = ', '.join([f"{col} {dtype}" for col, dtype in columns.items()])
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_definition})")
+        self.conn.commit()
+        print(f"Tabla '{table_name}' creada con columnas: {', '.join(columns.keys())}.")
+
+    def list_tables(self):
+        """ Devuelve una lista de los nombres de las tablas en la base de datos. """
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        return [table[0] for table in cursor.fetchall()]
+    
+    # Nota: La función create_or_connect_db no debería estar en este módulo
 
 
-def create_new_table(df, selected_columns):
-    return df[selected_columns]
 
-def merge_dataframes(df1, df2, on_column):
-    return pd.merge(df1, df2, on=on_column)
